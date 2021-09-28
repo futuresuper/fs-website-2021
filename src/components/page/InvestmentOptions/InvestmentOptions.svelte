12a7b3c664266e3862a6ad06b02cd444e5cd4845
Andrew <andrew@myfuturesuper.com.au>
2021-09-28 11:43:34 +1000

    fix: incorrect path

--- src/components/page/InvestmentOptions/InvestmentOptions.svelte
+++ src/components/page/InvestmentOptions/InvestmentOptions.svelte
@@ -1,13 +1,11 @@
 <script>
   import Summary from "./Summary.svelte";
   import Tables from "./Tables.svelte";
-  import LottieTest from "./LottieTest.svelte";
 </script>
 
 <div>
   <Summary />
   <Tables />
-  <LottieTest />
 </div>
 
 <style lang="scss">
