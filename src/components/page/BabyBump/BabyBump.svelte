e24b552311b8cd8373548731d42a8fb7dbaee461
Andrew <andrew@myfuturesuper.com.au>
2021-10-13 16:07:03 +1100

    feat: babybump

---
+++ src/components/page/BabyBump/BabyBump.svelte
@@ -0,0 +1,82 @@
+<section>
+  <p>
+    Baby Bump is a refund of the annual dollar-based administration fee for the
+    time you are on parental leave, up to a maximum of 12 months (equivalent to
+    $60).
+  </p>
+  <p>We created Baby Bump for two reasons:</p>
+  <ul>
+    <li>Employers don't have to pay super when you take parental leave</li>
+    <li>
+      Primary carers (who are historically mostly women) are more likely to take
+      parental leave than other carers
+    </li>
+  </ul>
+  <p>
+    These two factors contribute to the startling gender inequality in super:
+    that, on average, women retire with 42%* less super than men. So Baby Bump
+    is Future Super’s baby step towards making super more fair for our members.
+  </p>
+  <p>
+    You don't have to be the birth parent to take advantage of our Baby Bump
+    policy however. If you have welcomed a new child into your family and taken
+    parental leave or left work to care for that child, you can apply for the
+    Baby Bump regardless of your gender.
+  </p>
+  <p>
+    See <a
+      href="https://www.abc.net.au/radio/programs/nightlife/first-hour-finance/13310718"
+      >Closing the super gender gap</a
+    >.
+  </p>
+  <h2>Am I eligible?</h2>
+  <p>You may be eligible for the Baby Bump fee refund if:</p>
+  <ul>
+    <li>You are a Future Super member</li>
+    <li>
+      A new child entered into your care within the past 18 months, whether by
+      birth or adoption, or foster care
+    </li>
+    <li>
+      You have taken parental leave or left work to care for your new child
+    </li>
+  </ul>
+  <p>
+    To confirm eligibility, we do require some paperwork to confirm the birth,
+    adoption, or foster care of your new child. You can provide this
+    documentation after submitting your Baby Bump application below.
+  </p>
+  <h2>When should I apply?</h2>
+  <p>
+    ‍A Baby Bump application should be submitted when you return to work after
+    parental leave (or start working more than 10 hours per week).
+  </p>
+  <p>
+    Making the switch to full-time parent? You can still apply! If you're not
+    returning to work or planning to work more than 10 hours per week, apply for
+    the full Baby Bump refund after the child has been in your care for 12
+    months, or the child has left your care if you are a foster carer.
+  </p>
+  <p>
+    Please note that Baby Bump applications must be received within 18 months of
+    your new child's birth, adoption, or commencement of foster care.
+  </p>
+</section>
+
+<style lang="scss">
+  @use "../../../styles/" as *;
+
+  section {
+    margin: 60px 15vw;
+  }
+
+  h2 {
+    margin-top: 60px;
+  }
+
+  @media (max-width: 800px) {
+    section {
+      margin: 60px 0;
+    }
+  }
+</style>
---
+++ src/components/page/BabyBump/BabyBumpForm.svelte
@@ -0,0 +1,122 @@
+<form name="BabyBump" method="POST" data-netlify="true">
+  <h3>Apply for Baby Bump</h3>
+  <p>
+    <label>Name<input type="text" name="Name" /></label>
+  </p>
+  <p>
+    <label>Member Number<input type="text" name="Member Number" /></label>
+  </p>
+  <p>
+    <label>Email<input type="email" name="Email" /></label>
+  </p>
+  <p>How has your new child come into your care?</p>
+  <div class="radio">
+    <div>
+      <input type="radio" name="How came into care" value="birth" />
+      <label for="birth">Birth</label>
+    </div>
+    <div>
+      <input type="radio" name="How came into care" value="adoption" />
+      <label for="adoption">Adoption</label>
+    </div>
+  </div>
+  <div class="radio" />
+  <p>What date did this child come into your care?</p>
+  <input type="text" name="Date came into care" placeholder="DD/MM/YYYY" />
+  <p>Which of these apply to you?</p>
+  <div class="radio">
+    <div>
+      <input
+        type="radio"
+        name="Which of these apply to you"
+        value="Full Time Parent"
+      />
+      <label for="Full Time Parent"
+        >Full-time parent OR returning to work for less than 10 hours per week
+        (12 or more months since birth/adoption)
+      </label>
+    </div>
+    <div>
+      <input
+        type="radio"
+        name="Which of these apply to you"
+        value="Returning To Work"
+      />
+      <label for="Returning To Work"
+        >Returning to work for 10 or more hours per week</label
+      >
+    </div>
+  </div>
+
+  <p>What date did you start your parental leave?</p>
+  <input
+    type="text"
+    name="Date started parental leave"
+    placeholder="DD/MM/YYYY"
+  />
+  <p>What date did you end your parental leave?</p>
+  <input
+    type="text"
+    name="Date ended parental leave"
+    placeholder="DD/MM/YYYY"
+  />
+  <p>Did you work during your parental leave?</p>
+  <div class="radio">
+    <div>
+      <input type="radio" name="Did you work" value="yes" />
+      <label for="yes">Yes</label>
+    </div>
+    <div>
+      <input type="radio" name="Did you work" value="no" />
+      <label for="no">No</label>
+    </div>
+  </div>
+
+  <p>
+    If yes, did you work 10 hours or more in a single week throughout this time?
+    If so, how many weeks did you work 10 hours or more?
+  </p>
+  <input type="text" name="How many weeks worked 10 hours or more" />
+  <p class="disclaimer">
+    If you are deemed eligible, we will request a copy of your child’s birth
+    certificate or adoption papers before the rebate is paid. All information
+    collected will be stored and used in line with our
+    <a href="/privacy-policy">Privacy Policy</a>.
+  </p>
+  <p>
+    <button type="submit" class="primary">Submit</button>
+  </p>
+</form>
+
+<style lang="scss">
+  @use "../../../styles/" as *;
+
+  form {
+    margin: 60px 15vw;
+    border: 1px solid $black;
+    border-radius: 32px;
+    padding: 40px;
+  }
+
+  .radio {
+    margin-bottom: 40px;
+  }
+
+  input[type="text"],
+  input[type="email"] {
+    display: block;
+    padding: 8px;
+    border-radius: 8px;
+    border: 1px solid $black;
+    outline: none;
+    margin-top: 8px;
+    margin-bottom: 40px;
+    width: 100%;
+  }
+
+  @media (max-width: 800px) {
+    form {
+      margin: 60px 0;
+    }
+  }
+</style>
