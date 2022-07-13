b512565630342277483e0f4355063d1946f2d1c8
Andrew Sellen <andrewmsellen@gmail.com>
2022-07-13 14:21:43 +1000

    feat: add insurance page

---
+++ src/components/page/Insurance/InsuranceHeader.svelte
@@ -0,0 +1,74 @@
+<script>
+  import SwitchShakeCircle from "../../images/SwitchShakeCircle.svelte";
+  const green = "#3DFA52";
+</script>
+
+<div class="container page-header-container">
+  <div class="title-and-text">
+    <h1>Insurance</h1>
+    <h3>
+      Here's what you need to know about our insurance offerings at Future
+      Super.
+    </h3>
+    <div class="grid">
+      <div class="left">
+        <p>
+          Future Super offers opt-in insurance. You can apply for cover for the
+          following types of insurance:
+        </p>
+        <ul>
+          <li>
+            <span>→</span>&nbsp;Death Only
+          </li>
+          <li>
+            <span>→</span>&nbsp;Death &amp; Total Permanent Disablement (TPD),
+            and
+          </li>
+          <li>
+            <span>→</span>&nbsp;Income Protection
+          </li>
+        </ul>
+      </div>
+      <div class="right">
+        <p>
+          Death Only and Death &amp; TPD cover helps provide you or your
+          dependents (as applicable) with a lump sum payment in the event that
+          you die, suffer a terminal illness, or you become totally and
+          permanently disabled (where you have Death &amp; TPD cover).
+        </p>
+        <p>
+          Income Protection cover provides you with a monthly income calculated
+          in accordance with the relevant insurance policy (&apos;Monthly
+          Benefit&apos;) in the event that you become Partially Disabled or
+          Totally Disabled and a Sickness or Injury causes you to be unable to
+          work and earn an income.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  @use "../../../styles/" as *;
+
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-gap: 40px;
+    margin-top: 40px;
+  }
+
+  ul {
+    line-height: 34px;
+  }
+  span {
+    color: $green;
+    margin-right: 0.5rem;
+  }
+
+  @media (max-width: 800px) {
+    .grid {
+      grid-template-columns: 100%;
+    }
+  }
+</style>
