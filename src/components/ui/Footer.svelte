d6447601701922decfdc9b93ddf8972f1f9b3af1
Andrew Sellen <andrewmsellen@gmail.com>
2022-07-13 12:30:53 +1000

    feat: add fund info to footer

--- src/components/ui/Footer.svelte
+++ src/components/ui/Footer.svelte
@@ -68,20 +68,41 @@
         {/each}
       </div>
       <div class="horizontal-space-between">
-        <p class="disclaimer">
-          You should read the <a href="https://www.futuresuper.com.au/pds"
-            >PDS,</a
-          > <a href="https://www.futuresuper.com.au/tmd">TMD</a> and
-          <a href="https://www.futuresuper.com.au/fsg"
-            >Financial Services Guide</a
-          >
-          before making any financial decisions regarding Future Super. Information
-          provided is of a general nature only and we have not taken your personal
-          financial objectives, situation or needs into account. We recommend you
-          seek financial advice when considering if Future Super is right for your
-          objectives and needs. When considering returns, past performance is not
-          indicative of future performance.
-        </p>
+        <div>
+          <p class="disclaimer">
+            You should read the <a href="https://www.futuresuper.com.au/pds"
+              >PDS,</a
+            > <a href="https://www.futuresuper.com.au/tmd">TMD</a> and
+            <a href="https://www.futuresuper.com.au/fsg"
+              >Financial Services Guide</a
+            >
+            before making any financial decisions regarding Future Super. Information
+            provided is of a general nature only and we have not taken your personal
+            financial objectives, situation or needs into account. We recommend you
+            seek financial advice when considering if Future Super is right for your
+            objectives and needs. When considering returns, past performance is not
+            indicative of future performance.
+          </p>
+          <p class="disclaimer">
+            Interests in Future Super are issued by Diversa Trustees Limited
+            (ABN 49 006 421 638; AFSL 235153; RSE Licence L0000635) (“the
+            Trustee”) as trustee of the Future Super Fund (ABN 45 960 194 277;
+            RSE Registration R1072914) (“the Fund” or “Future Super”). The Fund
+            is administered by OneVue Super Services Pty Limited (ABN 74 006 877
+            872; AFSL 246883). Insurance cover is provided to eligible members
+            by AIA Australia Limited (ABN 79 004 837 861; AFSL 230043).
+          </p>
+          <p class="disclaimer">
+            The Founder, Promoter and Investment Manager of the Fund is Future
+            Super Investment Services Pty Ltd (ABN 55 621 040 702; AFS
+            Representative No. 001271441), which is a Corporate Authorised
+            Representative of Future Superannuation Holdings Pty Ltd (ABN 90 167
+            800 580; AFSL 482684). The Trustee does not in any way endorse,
+            warrant or accept responsibility for any services provided by the
+            Promoter in its own right or directly to members or prospective
+            members.
+          </p>
+        </div>
         <div class="bcorp">
           <BCorpLogo />
         </div>
@@ -224,7 +245,7 @@
   }
 
   .bcorp {
-    width: 48px;
+    width: 80px;
   }
 
   .acknowledgement {
@@ -247,6 +268,7 @@
   .horizontal-space-between {
     width: 100%;
     display: flex;
+    flex-direction: row;
     justify-content: space-between;
     align-items: flex-start;
   }
@@ -256,7 +278,7 @@
   }
 
   .disclaimer {
-    width: 40%;
+    width: 70%;
     color: $black500;
     a {
       &:hover {
