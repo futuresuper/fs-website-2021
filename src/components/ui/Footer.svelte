cae50cedfb88dbc2b29d532ede8925a0adc8fd33
Parabol Ink <richardnheck@gmail.com>
2022-12-15 15:46:02 +1100

    refactor: moved the information into a component to prevent duplication NMS-164

--- src/components/ui/Footer.svelte
+++ src/components/ui/Footer.svelte
@@ -8,6 +8,7 @@
   import SocialInsta from "../images/SocialInsta.svelte";
   import SocialLinkedIn from "../images/SocialLinkedIn.svelte";
   import Info from "../images/Info.svelte";
+  import EndOfYearInfo from "../common/EndOfYearInfo.svelte";
 
   const menu = [
     {
@@ -63,16 +64,9 @@
           <div class="pageGroup">
             <h5>{page.heading}</h5>
             <hr />
-            {#if page.heading === 'Company'}
-              <div class="info-block">
-                <div>
-                  <Info/>
-                </div>
-                <p><strong>Our office will be closed from Friday 23rd of December 2022 to Monday 2nd of January 2023, inclusive.</strong> We will reopen at 10am AEDT Tuesday 3rd January.
-                  <br/>Our Member Advocacy team will continue to monitor our inbox during this time to assist with any urgent enquiries.
-                </p>
-              </div>
-              {/if}
+            {#if page.heading === "Company"}
+              <EndOfYearInfo />
+            {/if}
             {#each page.subPages as subPage}
               <a href={subPage[1]}>{subPage[0]}</a>
             {/each}
@@ -200,21 +194,6 @@
 
 <style lang="scss">
   @use "../../styles/" as *;
-
-  .info-block{
-    background-color: #CEDEFF;
-    padding: 1rem;
-    border-radius: 0.5rem;
-    font-size: 0.875rem;
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    color: $black;
-    margin-bottom: 1.375rem;
-    p{
-      margin: 0;
-    }
-  }
 
   footer {
     margin-top: 80px;
