21cc1ce7ea832dce81241064ef87d89dcdc904de
Andrew Sellen <andrewmsellen@gmail.com>
2023-02-10 08:33:50 +1100

    feat: project cards

---
+++ src/components/images/Blob.svelte
@@ -0,0 +1,39 @@
+<svg
+  width="100%"
+  height="100%"
+  viewBox="0 0 1391 760"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <g filter="url(#filter0_f_1_1050)">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M667.625 517.068C746.872 479.68 838.51 464.345 913.314 479.558C991.238 495.405 1060.89 535.63 1066.2 598.126C1071.12 656.084 972.019 705.392 937.588 765.449C892.975 843.264 940.215 948.22 839.824 992.978C741.861 1036.65 665.111 957.915 589.946 920.439C536.294 893.689 488.779 861.713 476.115 811.637C464.252 764.724 492.174 713.041 524.804 662.851C560.373 608.142 597.069 550.355 667.625 517.068Z"
+      fill="#00A900"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_f_1_1050"
+      x="0.333008"
+      y="0.737305"
+      width="1539.04"
+      height="1478.08"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="BackgroundImageFix"
+        result="shape"
+      />
+      <feGaussianBlur
+        stdDeviation="236.5"
+        result="effect1_foregroundBlur_1_1050"
+      />
+    </filter>
+  </defs>
+</svg>
--- src/components/page/RenewablesProjects/RenewablesProjects.svelte
+++ src/components/page/RenewablesProjects/RenewablesProjects.svelte
@@ -1,12 +1,123 @@
 <script>
+  import Title from "./Title.svelte";
+  import Logo from "../../images/Logo.svelte";
+  import ProjectCard from "./ProjectCard.svelte";
+
+  const projects = [
+    {
+      projectName: "Brigalow Solar Farm",
+      mainImageUrl:
+        "https://images.ctfassets.net/phy8w0z2ccbs/1N0w0kGV1q3i2F8fQox5Lq/2ad479297a3763c4d352af9fdeb79ae7/brigalow2.png",
+      description:
+        "The Brigalow solar farm in Queensland, is located about 30 minutes rive south west of Toowoomba. It is expected to displace fossil fuel generation that will avoid 580 respiratory illnesses each year over the solar farm’s expected operating life.",
+      stateLocatedIn: "QLD",
+      renewablesType: "Utility Scale Solar",
+      readMoreLink:
+        "https://reneweconomy.com.au/impact-completes-brigalow-solar-farm-now-looking-at-project-financing/",
+      powerInMegawatts: 35,
+      homesPowered: 10550,
+      location: {
+        latitude: -26.8442,
+        longitude: 150.7909,
+      },
+      __type: "renewablesProject",
+    },
+    {
+      projectName: "Murra Warra Wind Farm - Stage 2",
+      mainImageUrl:
+        "https://images.ctfassets.net/phy8w0z2ccbs/7poFdmG6woKK5PCiN0Bb3A/4aadea229fe1c679ffb516b1a76f2828/murrawarra.png",
+      description:
+        "The Murra Warra Wind Farm project is located in North Western Victoria. Situated on 4250Ha of farmland used for sheep grazing and broadacre cropping, the wind farm footprint is less than 2% of the area. In addition to its minimal effects on agricultural productivity, Murra Warra was chosen for its good wind resource, close transport links, and an on-site connection to the grid network.",
+      stateLocatedIn: "VIC",
+      renewablesType: "Utility Scale Wind",
+      readMoreLink:
+        "http://murrawarrawindfarm.com/press-release-murra-warra-ii-wind-farm-to-commence-construction-following-successful-completion-of-development-phase-and-financial-close/",
+      powerInMegawatts: 209,
+      homesPowered: 0,
+      location: {
+        latitude: -36.431016741520224,
+        longitude: 142.31436394604185,
+      },
+      __type: "renewablesProject",
+    },
+    {
+      projectName: "Ti Tree",
+      mainImageUrl:
+        "https://images.ctfassets.net/phy8w0z2ccbs/701oZIq0kvhCG6IaAeQLrB/d47818b58dfde6313fffdad365f932cc/ti-tree.png",
+      description:
+        "Ti Tree supplies three of Australia's most remote aboriginal townships with essential power. It has a sophisticated battery backup Grid Stability System (GSS) which maximises solar energy input.",
+      stateLocatedIn: "NT",
+      renewablesType: "Off-grid Solar and Battery",
+      readMoreLink: "https://epuron.com.au/solar/tkln-solar-ti-tree/",
+      powerInMegawatts: 0.323,
+      homesPowered: 0,
+      location: {
+        latitude: -22.126863612869425,
+        longitude: 133.4105681041717,
+      },
+      __type: "renewablesProject",
+    },
+    {
+      projectName: "Chinchilla Solar Farm",
+      mainImageUrl:
+        "https://images.ctfassets.net/phy8w0z2ccbs/3C05yeGK3bCkan2j01xP4M/58e1dfaf0d79895c9167fe7fbfcdaee2/chinchilla.png",
+      description:
+        "Chinchilla Solar Farm is located in southeast Queensland. Located on 73 ha of land it has approximately 61,000 solar panels. It commenced operations in July 2019 and is expected to produce 41,940 MWh in its first year of operation, enough to power the equivalent of approximately 7,200 Australian homes.",
+      stateLocatedIn: "QLD",
+      renewablesType: "Utility Scale Solar ",
+      readMoreLink: "https://diamondenergy.com.au/chinchilla-solar/",
+      powerInMegawatts: 19.9,
+      homesPowered: 7200,
+      location: {
+        latitude: -26.72172335364175,
+        longitude: 150.55161428861598,
+      },
+      __type: "renewablesProject",
+    },
+    {
+      projectName: "Swan Hill Solar Farm",
+      mainImageUrl:
+        "https://images.ctfassets.net/phy8w0z2ccbs/1dGBXPUsEkmJuMVQLYrCxB/47c9ba9d09be077327278a7a6944413c/swan-hill2.png",
+      description:
+        "Swan Hill Solar Farm is located in Northwest Victoria. Located on 38 ha of land, it has approximately 53,000 solar panels. It commenced operations in July 2018 and produced 35,400 MWh in its first year of operation, enough to power the equivalent of approximately 6,200 Australian homes.",
+      stateLocatedIn: "VIC",
+      renewablesType: "Utility Scale Solar",
+      readMoreLink: "https://diamondenergy.com.au/swan-hill-solar-farm/",
+      powerInMegawatts: 19.3,
+      homesPowered: 6200,
+      location: {
+        latitude: -35.3402,
+        longitude: 143.5441,
+      },
+      __type: "renewablesProject",
+    },
+  ];
 </script>
 
-<div class="bg">
-  <div class="container">
-    <h1>NIGHTINGALE Wurru Wurru Biik</h1>
-
-    <div class="hero">
-      <h2>YOUR SUPER IS DISRUPTING THE PROPERTY MARKET</h2>
+<div class="bg-outer">
+  <div class="bg">
+    <div class="header">
+      <Title />
+      <div class="logo">
+        <Logo colour="white" size="100%" />
+      </div>
+    </div>
+    <div class="card-container">
+      <div class="description">
+        <h3>
+          Explore some renewables projects Future Super is invested in. Tap on
+          each card for more information.
+        </h3>
+        <p>
+          Exposure in these projects is obtained through Future Super's
+          investment in Infradebt
+        </p>
+      </div>
+      <div class="cards">
+        {#each projects as project}
+          <ProjectCard {project} />
+        {/each}
+      </div>
     </div>
   </div>
 </div>
@@ -14,266 +125,49 @@
 <style lang="scss">
   @use "../../../styles/" as *;
 
-  $font-size-xs: max(10px, 0.9vw);
-  $font-size-s: max(13px, 1.2vw);
-  $font-size-m: max(16px, 1.8vw);
-  $font-size-l: max(38px, 4vw);
-  $font-size-xl: max(44px, 5vw);
+  h3,
+  p {
+    color: $white;
+  }
+
+  .bg-outer {
+    background-color: $black;
+  }
 
   .bg {
     width: 100%;
-    background-color: $black;
+    min-height: 200vh;
+    padding: 20px;
+    background: radial-gradient(
+      circle,
+      rgba(61, 250, 82, 0.4) 0%,
+      rgba(61, 250, 82, 0.03) 40%,
+      rgba(33, 33, 33, 1) 100%
+    );
   }
 
-  .container {
-    background-color: $black;
-    color: $white;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    max-width: 1200px;
-  }
-
-  .timeline-section {
-    padding: 100px 0 200px 0;
-  }
-
-  h1 {
-    font-family: $regular;
-    font-weight: 400;
-    letter-spacing: 1px;
-    font-size: $font-size-m;
-    text-transform: uppercase;
-  }
-
-  h2 {
-    color: $green;
-    font-size: $font-size-xl;
-    line-height: 100%;
-  }
-
-  h3 {
-    font-family: $regular;
-    color: $green;
-    font-size: $font-size-l;
-    line-height: 100%;
-    max-width: 600px;
-    margin-bottom: 60px;
-  }
-
-  p {
-    font-size: $font-size-m;
-  }
-
-  .circles {
-    display: flex;
-    flex-direction: row;
-    margin-bottom: 100px;
-  }
-
-  .circle {
-    --size: min(300px, 80vw);
-    height: var(--size);
-    width: var(--size);
-    border-radius: 50%;
-    background-color: $black;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid $white;
-    margin: 20px;
-    cursor: pointer;
-    font-size: max(17px, 1.4vw);
-    line-height: 120%;
-    text-transform: uppercase;
-    padding: 40px;
-    &.on {
-      background-color: $green;
-      border-color: $green;
-      color: $black;
-    }
-    p {
-      text-transform: none;
-      font-size: $font-size-s;
-      margin-top: 10px;
+  .header {
+    position: relative;
+    width: 100%;
+    .logo {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      width: 11%;
     }
   }
 
-  .disclaimer {
-    max-width: 800px;
-    color: $black200;
+  .description {
+    margin-bottom: 80px;
   }
 
-  .skinny {
-    max-width: 800px;
-  }
-
-  .tap {
-    color: $green;
-    font-size: $font-size-s;
-    margin-top: 60px;
-  }
-
-  .arrow {
-    height: 12vh;
-    margin: 80px;
+  .card-container {
+    padding: 100px;
   }
 
   .cards {
-    margin: 60px 0 120px 0;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 20px;
-    &.two-col {
-      grid-template-columns: 1fr 1fr;
-    }
-  }
-
-  .icon {
-    width: 80px;
-  }
-
-  .large-icon {
-    width: 140px;
-  }
-
-  .card {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    padding: 40px;
-    background-color: $black;
-    border-radius: 32px;
-    filter: drop-shadow(0px -6px 10px rgba(0, 0, 0, 0.2));
-  }
-
-  .small {
-    font-size: $font-size-s;
-  }
-
-  .nightingale-image {
-    margin: 40px auto 120px auto;
-    border-radius: 32px;
-    background: url("https://res.cloudinary.com/future-super/image/upload/f_auto,q_auto/v1639300170/nightingale-front.png");
-    background-position: bottom;
-    background-size: cover;
-    background-repeat: no-repeat;
-    height: 50vh;
-    width: 100%;
-    max-width: 600px;
-    &.apartment {
-      background: url("https://res.cloudinary.com/future-super/image/upload/f_auto,q_auto/v1639300170/nightingale-apartment.png");
-    }
-  }
-
-  .earth {
-    margin-bottom: 60px;
-  }
-
-  .cross {
-    margin: -60px 0 0px 0;
-  }
-
-  .roof {
-    width: 350px;
-    max-width: 100%;
-    margin: 80px 0 120px 0;
-  }
-
-  .financial {
-    width: 100%;
-    max-width: 600px;
-    margin: 40px 0 120px 0;
-  }
-
-  .brick {
-    height: 40vh;
-    width: 100%;
-    margin: -40px 0 20px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: url("https://res.cloudinary.com/future-super/image/upload/c_scale,q_auto,w_400/v1639299184/Brick.gif");
-    background-position: center;
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
-
-  .hero {
-    height: 60vh;
-    margin: 40px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: url("https://res.cloudinary.com/future-super/image/upload/c_scale,q_auto,w_400/v1639299184/bricks-and-coins.gif");
-    background-position: center;
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
-
-  .timeline {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    width: 100%;
-    max-width: 700px;
-    grid-column-gap: 20px;
-  }
-
-  .sub {
-    margin: -40px 0 40px 0;
-  }
-
-  .label {
-    font-size: $font-size-s;
-  }
-
-  progress {
-    appearance: none;
-    display: block;
-    width: 100%;
-    border: 1px solid $green;
-    border-radius: 12px;
-    height: 12px;
-    &::-webkit-progress-bar {
-      background-color: $black;
-      border-radius: 10px;
-    }
-    &::-webkit-progress-value {
-      background-color: $green;
-      border-radius: 10px;
-    }
-  }
-
-  @media (max-width: 1100px) {
-    .circles {
-      flex-direction: column;
-      margin-bottom: 60px;
-    }
-  }
-
-  @media (max-width: 900px) {
-    .cards {
-      margin: 80px -40px;
-      grid-template-columns: 100%;
-      grid-gap: 0;
-      .card {
-        border-bottom-right-radius: 0;
-        border-bottom-left-radius: 0;
-      }
-      &.two-col {
-        grid-template-columns: 100%;
-      }
-    }
-
-    .timeline-section {
-      padding: 20px 0 140px 0;
-    }
-
-    .mobile-hide {
-      display: none;
-    }
+    grid-gap: 40px;
   }
 </style>
