dd33c6c563a0edf97ad9671e8f544362b8586170
Andrew Sellen <andrewmsellen@gmail.com>
2023-03-01 11:46:36 +1100

    feat: compliance change - rmv app screenshot

--- src/components/page/RenewablesProjects/QrCodeCard.svelte
+++ src/components/page/RenewablesProjects/QrCodeCard.svelte
@@ -4,14 +4,28 @@
 </script>
 
 <Card style="justify-content: center">
+  <h3>
+    Download the <span>Future Super App</span> for more content like this!
+  </h3>
   <QrCodeContent />
 </Card>
 
 <style lang="scss">
   @use "../../../styles/" as *;
 
+  h3 {
+    color: $white;
+    font-family: $regular;
+    text-align: center;
+    font-size: 24px;
+    margin-bottom: 10px;
+    span {
+      color: $green;
+    }
+  }
+
   img {
-    width: 205px;
+    width: 180px;
     margin: 5px;
   }
 </style>
