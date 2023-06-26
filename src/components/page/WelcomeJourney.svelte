2e4f2dea12b5d5b8119c8f6800ff8f8a2f4eea2b
brodyhollins <brody@zero2one.co.za>
2023-06-26 12:01:45 +0200

    feat: got the first section block of info with dots and sticks to top on scroll

--- src/components/page/WelcomeJourney.svelte
+++ src/components/page/WelcomeJourney.svelte
@@ -5,19 +5,19 @@ const secondActiveSet = 11;
 </script>
 
 <div class="bg">
-  <div class="container">
+  <div class="">
     <section class="hero-section">
       <h1 class="hero-section__heading center">Welcome to the movement</h1>
 
       <div class="hero-section__content">
         <p>
-          By joining Future Super, you are joining a movement fighting for a future free from climate change and inequality. 
+          By joining Future Super, you’re also joining a movement! We’re fighting for a future free from climate change and inequality. 
         </p>
         <p>
-          Our movement harnesses the power of money. It’s easy to feel powerless when we think about the big issues facing the world, but when we put our money together we can be a force for change. 
+          And we’re putting our money where our mouth is. It’s easy to feel powerless when we think about the big issues facing the world, but the truth is - money has power. Together, we can be a force for change. 
         </p>
         <p>
-          Here are some numbers that we think prove change is possible. 
+          What does the power of money look like? Well...
         </p>
       </div>
     </section>
@@ -25,35 +25,38 @@ const secondActiveSet = 11;
     <section class="balance-section">
       <div class="balance-section__circle">
         <div class="balance-section__balance center">
-          <h2 class="balance-section__balance--heading">284,550K</h2>
+          <h2 class="balance-section__balance--heading">$320,150K</h2>
           <p class="balance-section__balance--description">Is the average super balance at retirement</p>
         </div>
       </div>
 
       <div class="balance-section__content">
-        <p>Sounds like a lot of money, right? But, compared with the total amount of Australians' superannuation savings, that adds up to $3.4 trillion looks like a tiny dot.</p>
+        <p>Sounds like a lot of money, right? But, all together Australia has $3.4 trillion worth of super savings. That $320,150 is just a tiny dot.</p>
       </div>
     </section>
 
-    <section class="journey-section">
+    <div class="journey-section">
         <div class="active-dot top-dot"></div>
-        <div class="dots-background">
-        </div>
 
-      <div class="active-dot-set active-dot-set__first">
-        {#each Array(firstActiveSet) as _, index (index)}
-        <div class="active-dot"></div>
-          {/each}
-      </div>
+          <section class="journey-section__block journey-section__block--1">
+            <p>
+              Now let’s think about more numbers. Did you know that just <span class="highlighted">7.2%</span> of that money, or $302 billion, could 100% fund Australia’s transition to renewable energy?
+            </p>
+          </section>
+          <div class="active-dot-set active-dot-set__first">
+            {#each Array(firstActiveSet) as _, index (index)}
+              <div class="active-dot"></div>
+            {/each}
+          </div>
 
-
-      <div class="active-dot-set active-dot-set__second">
-        {#each Array(secondActiveSet) as _, index (index)}
-          <div class="active-dot"></div>
-        {/each}
-      </div>
-
-    </section>
+<!--        <section>-->
+<!--          <div class="active-dot-set active-dot-set__second">-->
+<!--            {#each Array(secondActiveSet) as _, index (index)}-->
+<!--              <div class="active-dot"></div>-->
+<!--            {/each}-->
+<!--          </div>-->
+<!--        </section>-->
+    </div>
 
   </div>
 </div>
@@ -65,11 +68,11 @@ const secondActiveSet = 11;
     width: 100%;
     background-color: $black;
     color: #E0E0E0;
-    overflow: hidden;
   }
 
   .hero-section{
     position: relative;
+    overflow-x: clip;
 
     &::after, &::before{
       content: "";
@@ -116,6 +119,7 @@ const secondActiveSet = 11;
     min-height: 750px;
     display: flex;
     flex-direction: column-reverse;
+    overflow-x: clip;
 
     &__circle{
       position: absolute;
@@ -155,23 +159,54 @@ const secondActiveSet = 11;
   }
 
   .journey-section{
-    height: 1000px;
+    height: 8000px;
     position: relative;
-  }
+    width: 100%;
+    background-image: radial-gradient(#424242 8px, transparent 9px), radial-gradient(#424242 7px, transparent 9px);
+    background-size: 22.5px 22.5px;
+    background-position: 50% 0;
+    padding-top: 1px;
+
+
+    //Smaller dots
+    //background-image: radial-gradient(#424242 1px, transparent 2px), radial-gradient(#424242 2px, transparent 2px);
+    //background-size: 6px 6px;
+
+
+    transition: all 1s ease-in-out;
+
+
+      z-index: 2;
+      &__block{
+        position: sticky;
+        top: 0;
+        padding: 0.5rem 2.5rem;
+        background-color: rgba(21,21,21, 0.85);
+        font-size: 1.25rem;
+        p{
+          margin: 0;
+        }
+
+        &--1{
+          //position: absolute;
+          margin-top: 495px;
+        }
+      }
+
+
+    }
 
   .dots-background{
     position: absolute;
     height: 100%;
     width: 100%;
-    background-image: radial-gradient(#424242 8px, transparent 1%), radial-gradient(#424242 7px, transparent 1%);
+    background-image: radial-gradient(#424242 8px, transparent 9px), radial-gradient(#424242 7px, transparent 9px);
     background-size: 22.5px 22.5px;
     background-position: 50% 0;
-    top: 0;
-    left: 0;
 
 
     //Smaller dots
-    //background-image: radial-gradient(#424242 1px, transparent 1%), radial-gradient(#424242 2px, transparent 1%);
+    //background-image: radial-gradient(#424242 1px, transparent 2px), radial-gradient(#424242 2px, transparent 2px);
     //background-size: 6px 6px;
 
 
@@ -203,7 +238,7 @@ const secondActiveSet = 11;
     transform: translateX(-50%);
 
     &__first{
-      top: 116px;
+      margin-top: 22px;
       width: 288px;
     }
     &__second{
@@ -219,6 +254,10 @@ const secondActiveSet = 11;
     height: 250px;
     width: 100%;
     background-color: white;
+  }
+
+  .highlighted{
+    color: $green;
   }
 
 </style>
