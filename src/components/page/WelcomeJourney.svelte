a0910c3ac548adf79349703b14b86c41bf9d5eff
brodyhollins <brody@zero2one.co.za>
2023-06-26 15:51:30 +0200

    feat: added the other sections to stick to the top and got smaller dots working without animation

--- src/components/page/WelcomeJourney.svelte
+++ src/components/page/WelcomeJourney.svelte
@@ -2,6 +2,16 @@
 const firstActiveSet = 37;
 const secondActiveSet = 11;
 
+//58 per, 41 last
+
+// 64 totla
+const thirdActiveSet = 273;
+
+
+const fourthActiveSet = 11;
+const fifthActiveSet = 11;
+const sixthActiveSet = 11;
+
 </script>
 
 <div class="bg">
@@ -43,19 +53,77 @@ const secondActiveSet = 11;
               Now let’s think about more numbers. Did you know that just <span class="highlighted">7.2%</span> of that money, or $302 billion, could 100% fund Australia’s transition to renewable energy?
             </p>
           </section>
-          <div class="active-dot-set active-dot-set__first">
+          <div class="active-dot-set">
             {#each Array(firstActiveSet) as _, index (index)}
               <div class="active-dot"></div>
             {/each}
           </div>
 
-<!--        <section>-->
-<!--          <div class="active-dot-set active-dot-set__second">-->
-<!--            {#each Array(secondActiveSet) as _, index (index)}-->
-<!--              <div class="active-dot"></div>-->
-<!--            {/each}-->
-<!--          </div>-->
-<!--        </section>-->
+
+      <section class="journey-section__block journey-section__block--2">
+        <p>
+          If we wanted to do even better than 100% renewables, and decarbonise the entire Australian economy - including transitioning transport and industry away from fossil fuels - the bill would be about $754 billion. That’s just <span class="highlighted">2.3%</span> on top.
+        </p>
+      </section>
+      <div class="active-dot-set">
+        {#each Array(secondActiveSet) as _, index (index)}
+          <div class="active-dot"></div>
+        {/each}
+      </div>
+
+      <section class="journey-section__block journey-section__block--3">
+        <p>
+          For decades now, climate activists have been fighting to get institutions all over the world - including super funds - to divest.
+        </p>
+      </section>
+
+      <section class="journey-section__block journey-section__block--4">
+        <p>
+          What is divestment? It’s refusing to invest in the fossil fuel companies that are causing catastrophic climate change.
+        </p>
+      </section>
+      <section class="journey-section__block journey-section__block--5">
+        <p>
+          In Australia, some super funds believe it’s possible to engage with coal, oil and gas companies to try to influence them to reduce emissions.
+        </p>
+      </section>
+      <section class="journey-section__block journey-section__block--6">
+        <p>
+          But at Future Super, we screen out fossil fuel companies from what we call our “investable universe”.
+        </p>
+      </section>
+      <section class="journey-section__block journey-section__block--7">
+        <p>
+          Future Super is just a tiny drop in the global divestment movement. 
+        </p>
+      </section>
+      <section class="journey-section__block journey-section__block--8">
+        <p>
+          The Divestment Database keeps track of all the institutions - like universities, charities and international pension funds - that have committed to divesting fossil fuels.
+        </p>
+      </section>
+      <section class="journey-section__block journey-section__block--9">
+        <p>
+          After so much scrolling, you might have forgotten - all these tiny dots are still representing that $3.4 trillion pool of super. And yep, that’s a lot of dots!
+        </p>
+      </section>
+      <section class="journey-section__block journey-section__block--10">
+        <p>
+          But $3.4 trillion is about to look like nothing. Wait until you see how big the divestment movement is....
+        </p>
+      </section>
+
+
+      <section class="journey-section__block journey-section__block--11">
+        <p>
+          All of these tiny dots make up the divestment movement. To offer some perspective - the wealth of Elon Musk, Jeff Bezos and Bill Gates combined is <span class="highlighted">$707 billion, that’s those green dots.</span>
+        </p>
+      </section>
+      <div class="active-dot-set active-dot-set__smaller">
+        {#each Array(thirdActiveSet) as _, index (index)}
+          <div class="active-dot active-dot__smaller"></div>
+        {/each}
+      </div>
     </div>
 
   </div>
@@ -162,56 +230,44 @@ const secondActiveSet = 11;
     height: 8000px;
     position: relative;
     width: 100%;
-    background-image: radial-gradient(#424242 8px, transparent 9px), radial-gradient(#424242 7px, transparent 9px);
-    background-size: 22.5px 22.5px;
+    background-image: radial-gradient(#424242 6px, transparent 9px), radial-gradient(#424242 8px, transparent 8px);
+    background-size: 24.5px 23.5px;
     background-position: 50% 0;
     padding-top: 1px;
 
 
+
     //Smaller dots
-    //background-image: radial-gradient(#424242 1px, transparent 2px), radial-gradient(#424242 2px, transparent 2px);
+    //background-image: radial-gradient(#424242 1.3px, transparent 1px), radial-gradient(#424242 1.3px, transparent 2px);
     //background-size: 6px 6px;
 
 
     transition: all 1s ease-in-out;
 
 
-      z-index: 2;
+      z-index: 1;
       &__block{
         position: sticky;
         top: 0;
         padding: 0.5rem 2.5rem;
         background-color: rgba(21,21,21, 0.85);
         font-size: 1.25rem;
+        z-index: 3;
         p{
           margin: 0;
         }
 
-        &--1{
-          //position: absolute;
+        &--1, &--3, &--4,&--5,&--6,&--7,&--8,&--9,&--10, &--11{
           margin-top: 495px;
+        }
+
+        &--2{
+          margin-top: 499px;
         }
       }
 
 
     }
-
-  .dots-background{
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    background-image: radial-gradient(#424242 8px, transparent 9px), radial-gradient(#424242 7px, transparent 9px);
-    background-size: 22.5px 22.5px;
-    background-position: 50% 0;
-
-
-    //Smaller dots
-    //background-image: radial-gradient(#424242 1px, transparent 2px), radial-gradient(#424242 2px, transparent 2px);
-    //background-size: 6px 6px;
-
-
-    transition: all 1s ease-in-out;
-  }
 
   .active-dot{
     width: 16.5px;
@@ -220,6 +276,11 @@ const secondActiveSet = 11;
     background-color: $green;
     position: relative;
     z-index: 2;
+
+    &__smaller{
+      width: 4px;
+      height: 4px;
+    }
   }
 
   .top-dot{
@@ -231,20 +292,19 @@ const secondActiveSet = 11;
 
   .active-dot-set{
     display: flex;
-    gap: 6px;
+    gap: 8px;
     flex-wrap: wrap;
     position: absolute;
     left: 50%;
     transform: translateX(-50%);
+    z-index: 2;
+    margin-top: 28px;
+    width: 312px;
 
-    &__first{
-      margin-top: 22px;
-      width: 288px;
-    }
-    &__second{
-      top: 206px;
-
-      width: 288px;
+    &__smaller{
+      gap: 2px;
+      margin-top: 21px;
+      width: 341px;
     }
   }
 
