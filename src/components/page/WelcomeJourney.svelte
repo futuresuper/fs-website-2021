eb1ce77aa295fb005fd25e7379ddb90aff2370ce
brodyhollins <brody@zero2one.co.za>
2023-08-04 13:58:53 +0200

    style: font changes for desktop

--- src/components/page/WelcomeJourney.svelte
+++ src/components/page/WelcomeJourney.svelte
@@ -347,7 +347,7 @@
         <div class="explain-section explain-section--first">
           <div class="explain-section__content hasArrow">
             <div>
-              <svg width="24" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M3 12L12 5L21 12V23C21 23.5304 20.7893 24.0391 20.4142 24.4142C20.0391 24.7893 19.5304 25 19 25H5C4.46957 25 3.96086 24.7893 3.58579 24.4142C3.21071 24.0391 3 23.5304 3 23V12Z" stroke="#3DFA52" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 <path d="M9 25V17H15V25" stroke="#3DFA52" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
@@ -370,7 +370,7 @@
         <div id="explainSection2" class="explain-section explain-section--second">
           <div class="explain-section__content hasArrow">
             <div>
-              <svg width="24" height="28" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg  viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g id="dollar-sign">
                   <path id="Vector" d="M12 2V22" stroke="#3DFA52" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   <path id="Vector_2" d="M17 5H9.5C8.57174 5 7.6815 5.36875 7.02513 6.02513C6.36875 6.6815 6 7.57174 6 8.5C6 9.42826 6.36875 10.3185 7.02513 10.9749C7.6815 11.6313 8.57174 12 9.5 12H14.5C15.4283 12 16.3185 12.3687 16.9749 13.0251C17.6313 13.6815 18 14.5717 18 15.5C18 16.4283 17.6313 17.3185 16.9749 17.9749C16.3185 18.6313 15.4283 19 14.5 19H6" stroke="#3DFA52" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -397,7 +397,7 @@
         <div id="explainSection3" class="explain-section explain-section--last">
           <div class="explain-section__content hasArrow">
             <div>
-              <svg width="24" height="28" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g id="calendar-clock">
                   <path id="Vector" d="M21 7.5V6C21 5.46957 20.7893 4.96086 20.4142 4.58579C20.0391 4.21071 19.5304 4 19 4H5C4.46957 4 3.96086 4.21071 3.58579 4.58579C3.21071 4.96086 3 5.46957 3 6V20C3 20.5304 3.21071 21.0391 3.58579 21.4142C3.96086 21.7893 4.46957 22 5 22H8.5" stroke="#3DFA52" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   <path id="Vector_2" d="M16 2V6" stroke="#3DFA52" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -991,6 +991,13 @@
       justify-content: center;
 
       animation: fadeIn 0.5s ease-in-out;
+
+      @media (min-width: 864px) {
+        font-size: 5.625rem;
+        br{
+          display: none;
+        }
+      }
     }
 
     &__content{
@@ -999,6 +1006,13 @@
       animation: fadeIn 0.5s ease-in-out;
 
       position: relative;
+
+      @media (min-width: 864px) {
+        font-size: 1.75rem;
+        p{
+          margin: 0;
+        }
+      }
 
       &::after{
         content: "";
@@ -1093,16 +1107,36 @@
       gap: 0.5rem;
       width: 100%;
 
+      svg{
+        width: 24px;
+        height: 28px;
+
+        @media (min-width: 864px) {
+          width: 30px;
+          height: 36px;
+
+        }
+      }
+
       &--heading{
         cursor: pointer;
         margin: 0;
-        font-size: 28px;
+        font-size: 1.75rem;
         font-weight: bold;
         font-family: 'FutureSuperFeature';
+
+        @media (min-width: 864px) {
+          font-size: 2.5rem;
+
+        }
       }
 
       &--text{
-        font-size: 20px;
+        font-size: 1.25rem;
+
+        @media (min-width: 864px) {
+          font-size: 1.75rem;
+        }
       }
     }
   }
@@ -1250,11 +1284,15 @@
           }
 
           @media (min-width: 769px) {
-            margin-top: 82px;
+            margin-top: 88px;
+          }
+
+          @media (min-width: 958px) {
+            margin-top: 73px;
           }
 
           @media (min-width: 1024px) {
-            margin-top: 92px;
+            margin-top: 82px;
           }
 
           margin-top: 69px;
@@ -1283,8 +1321,16 @@
             margin-top: 108px;
           }
 
+          @media (min-width: 864px) {
+            margin-top: 113px;
+          }
+
+          @media (min-width: 949px) {
+            margin-top: 99px;
+          }
+
           @media (min-width: 1024px) {
-            margin-top: 120px;
+            margin-top: 107px;
           }
 
           margin-top: 45px;
@@ -1304,8 +1350,12 @@
             margin-top: 61px;
           }
 
+          @media (min-width: 864px) {
+            margin-top: 50px;
+          }
+
           @media (min-width: 1024px) {
-            margin-top: 74px;
+            margin-top: 62px;
           }
 
           margin-top: 49px;
@@ -1320,6 +1370,11 @@
         font-size: 1.25rem;
         z-index: 3;
         width: 100%;
+
+        @media (min-width: 864px) {
+          font-size: 1.75rem;
+
+        }
 
         @media (min-width: 768px) {
           text-align: center;
@@ -1434,8 +1489,18 @@
           margin-left: 4px;
         }
 
+        @media (min-width: 864px) {
+          margin-top: 17px;
+          margin-left: 4px;
+        }
+
+        @media (min-width: 906px) {
+          margin-top: 14px;
+          margin-left: 4px;
+        }
+
         @media (min-width: 1024px) {
-          margin-top: 20px;
+          margin-top: 22px;
           margin-left: 4px;
         }
 
@@ -1474,6 +1539,11 @@
 
         @media (min-width: 769px) {
           margin-top: 8px;
+          margin-left: 4px;
+        }
+
+        @media (min-width: 864px) {
+          margin-top: 13px;
           margin-left: 4px;
         }
 
@@ -1518,7 +1588,7 @@
         height: 100vh;
         aspect-ratio: 1 /1;
         @media (min-width: 900px) {
-          aspect-ratio: 1.156 /1;
+          aspect-ratio: 1.225 /1;
         }
         border-radius: 500px;
         transform-origin: bottom;
@@ -1561,7 +1631,7 @@
       &--2{
         @media (min-width: 900px) {
           aspect-ratio: 1 /1;
-          left: 6.7%;
+          left: 9.2%;
         }
         background-color: #303030;
         transform: scale(0.0567) translateZ(0);
@@ -1577,7 +1647,7 @@
       &--3{
         @media (min-width: 900px) {
           aspect-ratio: 1 /1;
-          left: 6.7%;
+          left: 9.2%;
         }
         background-color: $green;
         color: #212121;
@@ -1630,6 +1700,9 @@
     font-size: 1.25rem;
     background-color: #212121;
     position: relative;
+    @media (min-width: 864px) {
+      font-size: 1.75rem;
+    }
     &::before{
       content: "";
       position: absolute;
@@ -2037,6 +2110,10 @@
     &__content{
       font-size: 1.75rem;
       font-weight: bold;
+
+      @media (min-width: 864px) {
+        font-size: 3rem;
+      }
 
     }
   }
