689a33107dd7c56efa1aca6d64692cedddff7d5b
brodyhollins <brody@zero2one.co.za>
2023-09-05 14:14:39 +0200

    feat: switch between control and impact design

--- src/components/page/JoinNow.svelte
+++ src/components/page/JoinNow.svelte
@@ -2,32 +2,26 @@
   import { onMount } from "svelte";
   import Arrow from "@components/images/Arrow.svelte";
 
+  export let infoBlockGroup;
+
   const infoBlockGroups = {
-    CONTROL: "2023Jul Control",        // Standard join information
-    SUPERMATCH: "2023Jul SuperMatch",  // Join information tailored for Supermatch
+    CONTROL: "2023Sep Control",        // Standard join form
+    IMPACT: "2023Sep Impact",  // Join form with image/impact
   };
 
-  // Set its default to the orginal text
-  let infoBlockGroup = infoBlockGroups.CONTROL;
 
   onMount(async () => {
-    const rand = Math.random();
-
-    infoBlockGroup =
-      rand > 0.5 ? infoBlockGroups.CONTROL : infoBlockGroups.SUPERMATCH;
-
     // Track the details text display
     analytics.track("JoinNow ViewedByABTestParticipant", {
       infoBlockGroup,
     });
   });
 </script>
-
-<div class="join-form ">
-
-  <div class="join-form__image">
+{#if infoBlockGroup === infoBlockGroups.IMPACT}
+<div class="impact">
+  <div class="impact__image">
     <a class="button secondary back-button"><span class="back-button--icon"><Arrow direction="left" colour="white" /></span><span class="back-button--text">Back</span></a>
-    <div class="join-form__image--content">
+    <div class="impact__image--content">
       <p>Join more</p>
       <p>than 40,000</p>
       <p>people</p>
@@ -37,9 +31,9 @@
     </div>
   </div>
 
-  <form class="join-form__form" method="GET" action="https://join.futuresuper.com.au/">
-    <div class="join-form__form--container">
-      <h2 class="join-form__form--heading">Join Future Super</h2>
+  <form class="impact__form" method="GET" action="https://join.futuresuper.com.au/">
+    <div class="impact__form--container">
+      <h2 class="impact__form--heading">Join Future Super</h2>
       <div class="time-row">
         <img src="/images/clock2.gif" alt="clock" class="clock" />
         <h4>Takes about 4 minutes.</h4>
@@ -48,11 +42,7 @@
         <p class="info-block__heading">What you'll need</p>
         <ul class="info-block__list">
           <li>Your Tax File Number</li>
-          {#if infoBlockGroup === infoBlockGroups.CONTROL}
-            <li>Current super details for any funds you plan to transfer*</li>
-          {:else}
-            <li>An Australian ID or Medicare card to find your super and complete a transfer*</li>
-          {/if}
+          <li>An Australian ID or Medicare card to find your super and complete a transfer*</li>
         </ul>
       </div>
       <p>
@@ -87,12 +77,56 @@
 
   </form>
 </div>
+{:else}
+  <form class="join-form" method="GET" action="https://join.futuresuper.com.au/">
+    <h1>Join Future Super</h1>
+    <div class="time-row">
+      <img src="/images/clock2.gif" alt="clock" class="clock" />
+      <h4>Joining takes about 4 minutes.</h4>
+    </div>
+    <div class="info-block">
+      <p class="info-block__heading">What you'll need</p>
+      <ul class="info-block__list">
+        <li>Your Tax File Number</li>
+        <li>An Australian ID or Medicare card to find your super and complete a transfer*</li>
+      </ul>
+    </div>
+    <p>
+      <label
+      >First Name<input
+              type="text"
+              id="first_name"
+              name="first_name"
+              required
+      /></label
+      >
+    </p>
+    <p>
+      <label>Email ^<input type="email" name="email" required /></label>
+    </p>
+    <input type="text" id="referer" name="ReferCode" style="display:none" />
+    <p>
+      <button type="submit" class="primary">Next →</button>
+    </p>
+    <p class="disclaimer">
+      * Please note that you don't need to transfer funds to create an account
+      with Future Super.<br /><br />
+      ^ By providing your email address, you consent and authorise us to send you communications
+      or information, including information required by law, via email or similar technologies.
+      Your details will never be passed onto a third party other than in accordance
+      with our <a href="/privacy-policy">Privacy Policy</a>. You can elect to
+      receive communications by post at any time by contacting Future Super on
+      1300 658 422 or via email at info@myfuturesuper.com.au or in writing at PO
+      Box 1282, Albury, NSW 2640.
+    </p>
+  </form>
+{/if}
 
 
 <style lang="scss">
   @use "../../styles/" as *;
 
-  .join-form{
+  .impact{
     display: flex;
     @media (max-width: 800px) {
       flex-direction: column;
@@ -170,6 +204,27 @@
 
         font-size: calc(30px + 1vw);
       }
+    }
+  }
+
+  .join-form{
+    margin: 20px 0 100px 0;
+    border: 1px solid $black;
+    border-radius: 32px;
+    padding: 40px;
+
+    h1 {
+      margin-bottom: 8px;
+    }
+
+    @media (min-width: 1300px) {
+      margin: 20px 50px 100px 50px;
+
+    }
+
+    @media (max-width: 800px) {
+      margin-top: -40px;
+
     }
   }
 
