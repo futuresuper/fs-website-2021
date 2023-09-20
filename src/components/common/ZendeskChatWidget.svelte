279057ede361a7d2a933b118882304d8869d8d89
Andrew Sellen <andrewmsellen@gmail.com>
2023-09-20 12:03:45 +1000

    feat: add zendesk chatbot

---
+++ src/components/common/ZendeskChatWidget.svelte
@@ -0,0 +1,8 @@
+<!-- Start of futuresuper Zendesk Widget script -->
+<div>
+  <script
+    id="ze-snippet"
+    src="https://static.zdassets.com/ekr/snippet.js?key=90d8b945-14c4-4425-a787-67421a617b22"
+  ></script>
+</div>
+<!-- End of futuresuper Zendesk Widget script -->
