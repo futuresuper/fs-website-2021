23cfd2407984f7dcf4bbc4028e97d06bf358106d
Andrew Sellen <andrewmsellen@gmail.com>
2023-11-01 14:53:34 +1100

    fix: mobile style of inv options page

--- src/components/page/InvestmentOptions/Tables.svelte
+++ src/components/page/InvestmentOptions/Tables.svelte
@@ -303,10 +303,15 @@
 
   @media (max-width: 760px) {
     .table-row {
-      grid-template-columns: repeat(3, 1fr);
+      grid-template-columns: repeat(4, 1fr);
       text-align: center;
       &.pension {
         grid-template-columns: 100%;
+      }
+
+      h4,
+      .row-head {
+        grid-column: span 4;
       }
     }
 
