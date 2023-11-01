120c0adf570fdeccdeeb48e40a0789b076f5a151
Andrew Sellen <andrewmsellen@gmail.com>
2023-11-02 08:35:39 +1100

    feat: update policy links to confluence

--- src/components/page/Purpose/Policies.svelte
+++ src/components/page/Purpose/Policies.svelte
@@ -27,7 +27,7 @@ To set people up for success working from home, we offer up to $1,000 budget to 
       description: `Future Super Group recognizes that menstrual and menopausal experiences can be debilitating, yet as a society, we have been indoctrinated to [deny its existence publicly](https://www.futuresuper.com.au/blog/a-bloody-good-policy/). All full-time Future Super Group employees who experience menstruation and menopause are entitled to 6 paid days off a year to manage the symptoms. 
 
 This policy also acts as a commitment by Future Super Group that as an organisation we aim to remove the stigma and taboo surrounding menstruation and menopause, and recognize its impact on women and all people that menstruate and experience menopause. `,
-      link: "https://docs.google.com/document/d/1bpX6CoxjewWIw2HHFs0eT0jgODYyrx5irUyu32JYEp8/edit",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Mental health and wellbeing",
@@ -36,7 +36,7 @@ This policy also acts as a commitment by Future Super Group that as an organisat
 All permanent employees are entitled to 6 paid days of mental health leave (or half a day a month) each year. This leave expires every 2 months rather than accruing, and that is deliberate because we actively encourage every employee to take it. 
 
 Achieving and maintaining good mental health and wellbeing is important for everyone and is an important part of how we work at Future Super. Mental illness or poor mental health is really common. In 2022, the [ABS reported](https://www.abs.gov.au/statistics/health/mental-health/national-study-mental-health-and-wellbeing/latest-release) that over two in five Australians aged 16-85 years (43.7% or 8.6 million people) had experienced a mental disorder at some time in their life. We acknowledge this and aim to always be a supportive workplace. We do this not only through our leave policy but by encouraging open discussion, making adjustments to work to support the mental health of our employees and treating workers with respect and fairness. `,
-      link: "https://drive.google.com/drive/folders/1j486oJO7b9BXI3YCUvry4DydQYnWAap7",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Ethical and local supplier guidelines",
@@ -45,23 +45,23 @@ Achieving and maintaining good mental health and wellbeing is important for ever
 That’s why we do not use suppliers that profit from, or have strong links to, fossil fuel companies. We also do not use suppliers that have animal cruelty in their supply chain or flout the Modern Slavery Act. 
 
 We prioritise giving our business to Aboriginal and/or Torres Strait Islander owned businesses or those certified with [Supply Nation](https://supplynation.org.au/?gclid=Cj0KCQjwl_SHBhCQARIsAFIFRVVvqwsRfwSrmL4euTn-DewZxeShVWEzbNG9zyIQSupvZHJZPWjCoNsaAmp3EALw_wcB). We also prioritise suppliers that use renewable energy, have environmentally friendly practices and those that are Certified B Corporations, social enterprises or not-for-profit organisations. `,
-      link: "https://drive.google.com/drive/folders/1tcvhHbQjkDDSN78qV-bMKzmdjMpHC7vU",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Inclusive Language Guide",
       description: `Future Super has an inclusive language guide which is constantly updated. The guide forms part of onboarding for all new employees and all employees are encouraged to provide feedback on it. Thinking about inclusive language is one of the ways Future Super aims to uphold its values and ethics. Showing consideration around language is one of the ways all businesses can create a more inclusive culture. The inclusive language guide is frequently updated and we are always learning and open to suggestions in this department. `,
-      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1836285953/Inclusive+Language+Guide",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Anti-racism policy",
       description:
         "There is zero tolerance for racism and discrimination at Future Super. The super fund’s mission statement is to build a future free from climate change and inequality, and celebrating differences and creating a psychologically safe space for employees is an important part of that. Future Super’s anti-racism policy aims to set the foundations for providing a safe space for those who have been marginalised by the injustices of systemic racism. We offer annual anti-racism training to all employees so they’re better informed about the origins of racism and better equipped to counter potential racism in the workplace, or in their personal life. Our training providers have included Hue, Democracy in Colour and Erfan Daliri. ",
-      link: "https://docs.google.com/document/d/1TNlPSKFSaQo2NeSMCIAQxEVd02xl73Qf/edit",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Climate emergency leave",
       description: `Future Super introduced this policy as a direct response to the bushfires of 2019/2020. With the impacts of climate change accelerating, we are seeing more and more frequent extreme weather events such as bushfires and floods. In the event a member of the team has been directly affected or displaced by catastrophic environmental events caused by climate change - or are volunteering for organisations that directly manage the response to these events, such as the Australian Red Cross, Rural Fire Service (RFS) and State Emergency Services (SES) - they can access unlimited paid leave.`,
-      link: "https://docs.google.com/document/d/1PyWWpfDu9_1YuSOC8VTgbKFoGMin1KtXN9CABSOJp_Y/edit",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Parental leave",
@@ -72,7 +72,7 @@ When taking parental leave, Future Super Group employees will continue to receiv
 To acknowledge parents’ need for flexibility, for the first three months after returning to work, parents can work 4 days per week  but be paid for a full 5-day week. If parents return to work and begin working part-time in a role that would otherwise be full-time were it not for their caring responsibilities, then they will receive super contributions as if they were working full-time. 
 
 There’s also no minimum requirement for length of employment to be eligible for parental leave at Future Super Group.`,
-      link: "https://docs.google.com/document/d/1XNnpa-Vnmq6GdkwN1PCybR8gt_my_8SFBLvs8R-KHjg/edit",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Gender affirmation leave",
@@ -87,7 +87,7 @@ Future Super will support Transgender and Gender Diverse employees by creating a
 Australia’s history is rooted in colonisation, exploitation, and injustice. Unfortunately, our current systems and norms are a product of historical inequalities, and continue to foster unfair outcomes for culturally and/or faith diverse people.
 
 With this policy, full-time culturally and/or faith diverse employees (either on a permanent or fixed-term contract) are entitled to up to 4 paid cultural leave days per financial year. Cultural leave is pro-rated. Managers are encouraged to facilitate cultural leave.`,
-      link: "https://docs.google.com/document/d/1HVWzDOAKajY2ECLWlkpppui-xRduxz8B/edit",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Cultural leave (for First Nations peoples)",
@@ -96,7 +96,7 @@ With this policy, full-time culturally and/or faith diverse employees (either on
 A [Minderoo Foundation study from 2018](https://www.minderoo.org/indigenous-employment-index/), found that racism in the workplace is a common experience for Indigenous employees, with more than 50% of study participants reporting they had experienced direct or indirect racism at work. The study also found that many First Nations employees feel culturally unsafe at work, meaning they cannot practise their cultural identity without discrimination, ridicule or denunciation.
 
 Full-time Aboriginal and Torres Strait Islander employees (either on a permanent or fixed-term contract) are entitled to up to 4 paid cultural leave days per financial year. Cultural leave is pro-rated. Managers are encouraged to facilitate cultural leave.`,
-      link: "https://docs.google.com/document/d/1EkVTFmmlSyt5O4LRydSXllSufIphRzo-/edit",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Public holiday swapping",
@@ -105,14 +105,14 @@ Full-time Aboriginal and Torres Strait Islander employees (either on a permanent
 All full-time and part-time employees (either on a permanent or fixed-term contract) are entitled to swap any number of public holidays within a calendar year. The public holiday(s) can be swapped for any other working day in the same calendar year.
 
 Christmas Day, Boxing Day and New Year’s Day are currently excluded from this policy, due to the office shutdown period.`,
-      link: "https://docs.google.com/document/d/1MoBOvZC9lBYOiz5IkXNwCREKZvDYCWCfeZ7BhAPb1dU/edit",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Pay the Rent",
       description: `In acknowledging that the sovereignty of First Nations people was never ceded, we also acknowledge that we conduct business and take shelter on land which was removed from First Nations people. This has created a disproportionate distribution of wealth and marginalised the Aboriginal and Torres Strait Islander owners. 
 
 Paying the Rent is a step towards justice, truth and equality for First Nations people. Future Super Group pays rent through the [Pay The Rent Collective](https://paytherent.net.au/about-us/), which disburses payments to a range of Aboriginal and Torres Strait Islander organisations they partner with across the country.`,
-      link: "https://docs.google.com/document/d/1la0VZhGWGBTRIud4qYXJfbvnDhnAD_m0/edit",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
     {
       title: "Statement of commitment to increase First Nations employment",
@@ -121,7 +121,7 @@ Paying the Rent is a step towards justice, truth and equality for First Nations 
 This figure is in-line with the 2021 census statistic that 3.8% of the Australian population are Aboriginal and Torres Strait Islander.
 
 The commitment to increasing First Nations employment was developed in consultation with Arrilla Indigenous Consulting (Arrilla), which created an Aboriginal and Torres Strait Islander Employment Strategy for Future Super Group. We acknowledge that there is a lot of work to be done internally to attract and maintain First Nations employees and, thanks to insights from Arrilla, we have steps in place to meet our target.`,
-      link: "https://docs.google.com/document/d/1d55QtU07ydvr-mPZprTl56dC3ThOCvnuNiaElsnqVfM/edit",
+      link: "https://myfuturesuper.atlassian.net/wiki/spaces/Public/pages/1947730015/Future+Group+s+Open+Source+Policies+and+Guidelines",
     },
   ];
 </script>
