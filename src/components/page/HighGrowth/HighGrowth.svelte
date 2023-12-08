5bf3b290a0ea5a7a161c2d4c7526f471abb0cf92
Andrew Sellen <andrewmsellen@gmail.com>
2023-12-08 16:50:42 +1100

    feat: high growth lp

--- src/components/page/HighGrowth/HighGrowth.svelte
+++ src/components/page/HighGrowth/HighGrowth.svelte
@@ -5,3 +5,12 @@
 
 <Hero />
 <LowDown />
+<div class="container">
+  <p>Disclaimers:</p>
+  <p>
+    *Investing in High Growth involves risks, do your research and consider
+    seeking financial advice based on your individual circumstances.
+  </p>
+  <p>^ CPI is a measure of inflation, 3.5% over any 12-year period.</p>
+  <p>**Survey from March 2023.</p>
+</div>
