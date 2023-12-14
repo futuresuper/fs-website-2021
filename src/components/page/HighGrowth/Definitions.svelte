ec2e23c8439e557c01ab4a6a953a49d40b7c1c92
Andrew Sellen <andrewmsellen@gmail.com>
2023-12-14 11:09:26 +1100

    feat: compliance updates

---
+++ src/components/page/HighGrowth/Definitions.svelte
@@ -0,0 +1,52 @@
+<div class="container">
+  <div class="box">
+    <h2>What on Earth does that mean?</h2>
+    <div>
+      <p>
+        <span class="bold"> Growth assets: </span>
+        Growth assets, like shares, have the potential to increase in value significantly
+        over time, but they are also subject to fluctuation.
+      </p>
+      <p>
+        <span class="bold"> Defensive assets: </span>
+        More stable assets, like cash and bonds. You can earn steady interest on
+        these investments with a lower chance of losing money. The tradeoff here
+        is steady returns are generally lower and there’s no potential for growth
+        beyond the expected returns.
+      </p>
+      <p>
+        <span class="bold"> Risk: </span>
+        This is the uncertainty, or market movement, your investment is subject to.
+        High Growth has been designed for individuals who are comfortable with navigating
+        short-term-market volatility in pursuit of higher long-term returns, therefore
+        there’s some risk involved.
+      </p>
+      <p>
+        For more information visit our
+        <a href="/how-we-invest"> How We Invest </a>
+        page.
+      </p>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .box {
+    display: grid;
+    padding: 40px;
+    border-radius: 30px;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 40px;
+    border: 1px solid black;
+
+    h2 {
+      text-wrap: balance;
+    }
+  }
+
+  @media (max-width: 768px) {
+    .box {
+      grid-template-columns: 1fr;
+    }
+  }
+</style>
