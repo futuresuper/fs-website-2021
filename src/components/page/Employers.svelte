2e8b85284b73f78fdea9ff663b4e151dc3faf325
Andrew Sellen <andrewmsellen@gmail.com>
2024-01-24 11:37:10 +1100

    feat: employer page updates

--- src/components/page/Employers.svelte
+++ src/components/page/Employers.svelte
@@ -1,36 +1,43 @@
 <section>
   <p>
-    The only way to manage your employees' super is by clearing house. Using a
-    clearing house ensures your payments are SuperStream compliant, and makes
-    them easy to track.
+    When you pay your employees’ contributions, they need to meet SuperStream
+    requirements.
   </p>
   <p>
-    You might already have access to a clearing house through your accounting
-    software. If not, the ATO's <a
-      href="https://www.ato.gov.au/Business/super-for-employers/paying-super-contributions/how-to-pay-super/small-business-superannuation-clearing-house/"
+    Using a clearing house ensures your payments are SuperStream compliant, and
+    makes them easy to track. If you use OneTouch Payroll software, you might
+    already have access to a clearing house through it. Small business with less
+    than 20 employees may be able to access the Australian Taxation Office’s
+    <a
+      href="https://www.ato.gov.au/businesses-and-organisations/super-for-employers/paying-super-contributions/how-to-pay-super/small-business-superannuation-clearing-house"
     >
       Small Business Superannuation Clearing House
-    </a> is free for small businesses with less than 20 employees.
-  </p>
-  <h3>Important Information - Rejected Contributions</h3>
-  <p>
-    In order to accept an employer contribution, our superstream system checks
-    your employees full names, date of birth, member number and TFNs with what
-    we have on file.
-  </p>
-  <p>
-    If any of your employee details do not match what we have on file for them,
-    your contribution will be rejected. This is due to an update on our systems
-    from 1 November. If you have previously been able to make contributions and
-    are now having them rejected, please check all the aforementioned details of
-    your employees are correct.
-  </p>
-  <p>
-    Please get in touch with your employee to make sure your details with them
-    are matching the details we have on file.
-  </p>
+    </a>
 
-  <h3>Our clearing house details</h3>
+    for free.
+  </p>
+  <p>
+    When we receive a Super Guarantee contribution from an employer, we need to
+    check the information sent with it matches the member’s Future Super
+    account.
+  </p>
+  <p>
+    Our system will only accept a contribution if the
+    <strong> member number</strong>,
+    <strong> last name/s</strong>,
+    <strong> date of birth (DOB)</strong>, and
+    <strong> Tax File Number (TFN)</strong> provided are an
+    <strong>
+      exact match for the member’s Future Super account information.
+    </strong>
+  </p>
+  <p>
+    Your employee can check their account details by logging in to their online
+    account or using the Future Super app. They can also reach out to our team
+    by emailing us at info@futuresuper.com.au, or by calling 1300 658 422 - see
+    our <a href="/contact-us">Contact Us</a> page for more details.
+  </p>
+  <h3>Our fund details are</h3>
   <div class="grid">
     <h4>Fund name</h4>
     <p>Future Super <span>or</span> Future Super Fund</p>
@@ -47,6 +54,61 @@
     <h4>Fund Address</h4>
     <p>GPO Box 2754, Brisbane QLD 4001</p>
   </div>
+
+  <p style="margin-top: 40px">
+    The letter to confirm we’re a
+    <strong> complying fund who will accept employer contributions </strong>
+    is our
+    <a href="/generalcompliancecertificate">
+      General Compliance Certificate.
+    </a>
+  </p>
+
+  <h3>Important information for rejected contributions</h3>
+
+  <p>
+    If you’ve had a Super Guarantee contribution returned to you since the end
+    of October 2023, it may be due to a failed verification check.
+  </p>
+  <p>
+    From 1 November 2023, our system has been updated with more robust checks to
+    verify member information. The most common mistake leading to returned
+    contribution are
+    <strong> typos in the name and/or date of birth, </strong>
+    and members who have legally changed their name but are yet to update their super
+    account.
+  </p>
+  <p>
+    We recommend that your employee checks the name and date of birth listed on
+    their online account, or verifies their details by contacting our team, to
+    help you compare against
+    <strong>
+      the details listed the employee file used by your payroll system or
+      clearing house.
+    </strong>
+  </p>
+
+  <p>
+    We also recommend ensuring you have the correct
+    <strong> member number </strong>
+    for the employee and are not using the account number associated with their superannuation
+    account.
+  </p>
+  <p>
+    Future Super’s bank account details were changed in in 2023. If your
+    employee’s details are correct, please confirm that your clearing house or
+    payroll system are using our correct and
+    <strong> up-to-date fund details </strong>
+    by checking that their system has synced with the Australian Taxation Office
+    (ATO)’s
+    <a
+      href="https://www.ato.gov.au/businesses-and-organisations/super-for-employers/paying-electronically-through-superstream/apra-regulated-funds/fund-validation-service"
+    >
+      Fund Validation Service (FVS)
+    </a>
+    register since
+    <strong> 1&nbsp;November&nbsp;2023. </strong>
+  </p>
 </section>
 
 <style lang="scss">
