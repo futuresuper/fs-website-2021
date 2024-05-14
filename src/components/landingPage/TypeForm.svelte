9075af185251b27c8c7c5317683f6c826fbd0500
zbrody <102783598+zbrody@users.noreply.github.com>
2024-05-14 09:26:13 +0200

    feat: add typeform component for landing pages (#694)
    FUM-343

---
+++ src/components/landingPage/TypeForm.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let section;
+</script>
+
+<div class="container">
+  <section class="wrapper" style="height: {section.height !== '' ? section.height : '100vh'}">
+    <div
+            id="wrapper"
+            class="wrapper"
+            data-tf-widget={section.typeform_id}
+            data-tf-inline-on-mobile
+            data-tf-medium="snippet"
+            data-tf-height={section.height}
+    />
+    <script src="//embed.typeform.com/next/embed.js"/>
+  </section>
+</div>
+
+<style lang="scss">
+  @use "../../styles/index" as *;
+
+  .wrapper{
+    width: 100%;
+    height: 100%;
+  }
+</style>
